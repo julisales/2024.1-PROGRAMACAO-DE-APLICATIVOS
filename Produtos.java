850b620d55bd1477d8060b67c6b96d1095fc97b2
Juli Sales <136769997+julisales@users.noreply.github.com>
2024-03-07 11:18:34 -0300

    Create Produtos.java

---
+++ Produtos.java
@@ -0,0 +1,19 @@
+public class Produtos
+{
+	public static void main(String[] args) {
+		
+		String[] produto = {"Manga", "Morango", "Laranja"};
+		
+        float emin, emax, vmd, tr, lr;
+        String td;
+        
+		System.out.println(produto[0]);
+        
+        float mes1 = 100, mes2 = 100, mes3 = 100;
+        
+        vmd = ((mes1 + mes2 + mes3)/3)/25
+        
+        System.out.println("A venda média diária é: " + vmd);
+            
+	}
+}

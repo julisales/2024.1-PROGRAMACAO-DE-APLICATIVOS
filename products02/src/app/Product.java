2dd9e373b51c3406593b6f8c758c3a061bef7b43
Juli Sales <136769997+julisales@users.noreply.github.com>
2024-04-13 21:24:57 -0300

    Add files via upload

---
+++ products02/src/app/Product.java
@@ -0,0 +1,15 @@
+package app;
+public class Product {
+
+    public String Name;
+    public double Quantity;
+    public double Price;
+
+    public void addProduct(int stock){
+        this.Quantity += stock;
+    }
+
+    public void subProduct(int stock){
+        this.Quantity -= stock;
+    }
+}
---
+++ products02/src/app/Program.java
@@ -0,0 +1,33 @@
+package app;
+
+import javax.swing.*;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Product product = new Product();
+
+        product.Name = JOptionPane.showInputDialog("Informe o produto que será cadastrado: ");
+
+        product.Quantity = Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade: "));
+
+        product.Price = Double.parseDouble(JOptionPane.showInputDialog("Informe o preço individual: "));
+
+        JOptionPane.showMessageDialog(null, "Confirmação: " + product.Name + ", Estoque atual: " + product.Quantity + ", Preço: " + product.Price);
+
+        int stock = Integer.parseInt(JOptionPane.showInputDialog("Deseja inserir mais produtos? Digite a quantidade: "));
+
+        product.addProduct(stock);
+
+        JOptionPane.showMessageDialog(null, "Atualização: " + product.Name + ", Estoque atual: " + product.Quantity + ", Preço: " + product.Price);
+
+        stock = Integer.parseInt(JOptionPane.showInputDialog("Atualmente alguma venda foi realizada? Digite a " +
+                "quantidade: "));
+
+        product.subProduct(stock);
+
+        JOptionPane.showMessageDialog(null,
+                "Atualização pós venda: " + product.Name + ", Estoque atual: " + product.Quantity + ", Preço: " + product.Price + ", Valor atual do estoque: " + product.Price * product.Quantity);
+    }
+}
+
